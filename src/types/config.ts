abda2ba9863d425d108854d67e3635b5556f362b
gentlementlegen <fernand.veyrier@epitech.eu>
2024-11-07 20:36:36 +0900

    fix: added config types

---
+++ src/types/config.ts
@@ -0,0 +1,3 @@
+export const CONFIG_FULL_PATH = ".github/.ubiquity-os.config.yml";
+export const DEV_CONFIG_FULL_PATH = ".github/.ubiquity-os.config.dev.yml";
+export const CONFIG_ORG_REPO = ".ubiquity-os";

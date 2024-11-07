a0479e373120b22d30b28510542904b7e4907807
gentlementlegen <fernand.veyrier@epitech.eu>
2024-11-07 20:05:13 +0900

    feat: remove Cypress tests, build scripts, and dependencies
    Removed all Cypress tests, build scripts, configurations, and related dependencies.

---
+++ src/types/manifest.ts
@@ -0,0 +1,19 @@
+import { type Static, Type as T } from "@sinclair/typebox";
+import { emitterEventNames } from "@octokit/webhooks";
+
+export const runEvent = T.Union(emitterEventNames.map((o) => T.Literal(o)));
+
+export const commandSchema = T.Object({
+  description: T.String({ minLength: 1 }),
+  "ubiquity:example": T.String({ minLength: 1 }),
+});
+
+export const manifestSchema = T.Object({
+  name: T.String({ minLength: 1 }),
+  description: T.Optional(T.String({ default: "" })),
+  commands: T.Optional(T.Record(T.String(), commandSchema, { default: {} })),
+  "ubiquity:listeners": T.Optional(T.Array(runEvent, { default: [] })),
+  configuration: T.Optional(T.Record(T.String(), T.Any(), { default: {} })),
+});
+
+export type Manifest = Static<typeof manifestSchema>;
---
+++ src/util.ts
@@ -0,0 +1,5 @@
+import { LogReturn } from "@ubiquity-os/ubiquity-os-logger";
+
+export function sanitizeMetadata(obj: LogReturn["metadata"]): string {
+  return JSON.stringify(obj, null, 2).replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/--/g, "&#45;&#45;");
+}

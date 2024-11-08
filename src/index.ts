516c6da87588b2e527b432e326860e2c4a7205a3
gentlementlegen <fernand.veyrier@epitech.eu>
2024-11-08 13:07:39 +0900

    fix: split exports into individual modules
    Separated constants and manifest exports, updated module resolution.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ export { createPlugin } from "./server";
 export { createActionsPlugin } from "./actions";
 export { postComment } from "./comment";
 export type { Context } from "./context";
-export * from "./constants";
-export type { Manifest } from "./types/manifest";

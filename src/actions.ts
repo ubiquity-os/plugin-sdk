4eedb7e8c152367d28e747d0b23f62e7ff6e8e9e
gentlementlegen <fernand.veyrier@epitech.eu>
2025-01-27 16:57:17 +0900

    chore: added core logs

--- src/actions.ts
+++ src/actions.ts
@@ -30,7 +30,7 @@ export async function createActionsPlugin<TConfig = unknown, TEnv = unknown, TCo
   const inputSchemaErrors = [...Value.Errors(inputSchema, body)];
   if (inputSchemaErrors.length) {
     console.dir(inputSchemaErrors, { depth: null });
-    core.setFailed(`Error: Invalid inputs payload: ${inputSchemaErrors.join(",")}`);
+    core.setFailed(`Error: Invalid inputs payload: ${inputSchemaErrors.map((o) => o.message).join(", ")}`);
     return;
   }
   const signature = body.signature;
@@ -46,6 +46,7 @@ export async function createActionsPlugin<TConfig = unknown, TEnv = unknown, TCo
       config = Value.Decode(pluginOptions.settingsSchema, Value.Default(pluginOptions.settingsSchema, inputs.settings));
     } catch (e) {
       console.dir(...Value.Errors(pluginOptions.settingsSchema, inputs.settings), { depth: null });
+      core.setFailed(`Error: Invalid settings provided.`);
       throw e;
     }
   } else {
@@ -58,6 +59,7 @@ export async function createActionsPlugin<TConfig = unknown, TEnv = unknown, TCo
       env = Value.Decode(pluginOptions.envSchema, Value.Default(pluginOptions.envSchema, process.env));
     } catch (e) {
       console.dir(...Value.Errors(pluginOptions.envSchema, process.env), { depth: null });
+      core.setFailed(`Error: Invalid environment provided.`);
       throw e;
     }
   } else {

334ea4cdb2a04096e7548f5b2845cf18ed667858
gentlementlegen <fernand.veyrier@epitech.eu>
2025-03-24 13:39:38 +0900

    fix: export Options type

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { createPlugin } from "./server";
 export { createActionsPlugin } from "./actions";
 export { CommentHandler } from "./comment";
 export type { Context } from "./context";
+export type { Options } from "./util";

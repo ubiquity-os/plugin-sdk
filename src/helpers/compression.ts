e75b7b228ebfe2b28eadc899093ca8c192f6b773
gentlementlegen <fernand.veyrier@epitech.eu>
2025-08-24 23:18:07 +0200

    fix: removed external brotli in favor of nodejs builtin

--- src/helpers/compression.ts
+++ src/helpers/compression.ts
@@ -1,13 +1,13 @@
-import { compress, decompress } from "brotli";
+import { brotliCompressSync, brotliDecompressSync } from "node:zlib";
 
 export function compressString(str: string): string {
   const input = Buffer.from(str, "utf8");
-  const compressed = compress(input);
+  const compressed = brotliCompressSync(input);
   return Buffer.from(compressed).toString("base64");
 }
 
 export function decompressString(compressed: string): string {
   const buffer = Buffer.from(compressed, "base64");
-  const decompressed = decompress(buffer);
+  const decompressed = brotliDecompressSync(buffer);
   return Buffer.from(decompressed).toString("utf8");
 }
